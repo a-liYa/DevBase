5529a5219f7614ab993512b407468df761f82a38
a_liYa <liyaf@8531.cn>
2018-11-26 20:27:38 +0800

    添加切换状态栏和导航栏背景示例

--- app/src/main/java/com/aliya/base/simpe/MainActivity.java
+++ app/src/main/java/com/aliya/base/simpe/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.aliya.base.simpe.ui.MainTabLayout;
-import com.aliya.base.simpe.ui.activity.SecondActivity;
+import com.aliya.base.simpe.ui.activity.NormalPageActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         getDecorChildView(0).setFitsSystemWindows(false);
         mTabLayout.setupBind(this, getSupportFragmentManager(), R.id.frame_layout);
         mTabLayout.setAdapter(new MainTabAdapterImpl());
-        startActivity(new Intent(this, SecondActivity.class));
+        startActivity(new Intent(this, NormalPageActivity.class));
         finish();
     }
 
---
+++ app/src/main/java/com/aliya/base/simpe/constant/Colors.java
@@ -0,0 +1,20 @@
+package com.aliya.base.simpe.constant;
+
+import com.aliya.base.simpe.R;
+
+/**
+ * 颜色值定义类
+ *
+ * @author a_liYa
+ * @date 2018/11/26 19:33.
+ */
+public class Colors {
+
+    public static final int[] COLOR_IDS = new int[]{
+            R.color.red, R.color.pink, R.color.purple, R.color.deep_purple,
+            R.color.indigo, R.color.blue, R.color.light_blue, R.color.cyan,
+            R.color.teal, R.color.green, R.color.light_blue, R.color.lime,
+            R.color.yellow, R.color.amber, R.color.orange, R.color.deep_orange,
+            R.color.brown, R.color.grey, R.color.blue_grey};
+
+}
