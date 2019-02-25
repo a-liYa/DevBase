b92ac23cae0e14027c5f7743d435aa59fb22d99a
a_liYa <liyafei0412@126.com>
2019-02-25 10:47:17 +0800

    添加IO流工具类

---
+++ devbase/src/main/java/com/aliya/base/util/IOs.java
@@ -0,0 +1,27 @@
+package com.aliya.base.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * IO流 相关处理工具类
+ *
+ * @author a_liYa
+ * @date 2019/2/25 10:41.
+ */
+public final class IOs {
+
+    public static void close(Closeable... closeables) {
+        if (closeables == null) return;
+        try {
+            for (Closeable closeable : closeables) {
+                if (closeable != null) {
+                    closeable.close();
+                }
+            }
+        } catch (IOException e) {
+            // no-op
+        }
+    }
+
+}
