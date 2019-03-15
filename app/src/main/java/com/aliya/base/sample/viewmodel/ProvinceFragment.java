498f918debd110571e828074d91f11d0fe237db4
a_liYa <liyafei0412@126.com>
2019-03-15 16:09:44 +0800

    删除无用Log

--- app/src/main/java/com/aliya/base/sample/viewmodel/ProvinceFragment.java
+++ app/src/main/java/com/aliya/base/sample/viewmodel/ProvinceFragment.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -62,7 +61,6 @@ public class ProvinceFragment extends BaseFragment implements OnItemClickListene
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(getActivity()).get(AreaSelectViewModel.class);
         mViewModel.getProvinces().observe(this, this);
-        Log.e("TAG", "onActivityCreated: " + mViewModel.hashCode());
     }
 
     @Override
