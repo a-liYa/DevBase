4907eec7f902105a741c7d6f960a5911644c6157
a_liYa <liyafei0412@126.com>
2019-05-21 21:53:08 +0800

    优化CrashCompat

--- devbase/src/main/java/com/aliya/base/compat/CrashCompat.java
+++ devbase/src/main/java/com/aliya/base/compat/CrashCompat.java
@@ -8,28 +8,32 @@ import java.util.concurrent.TimeoutException;
  * @author a_liYa
  * @date 2019/5/19 10:12.
  */
-public class CrashCompat {
+public final class CrashCompat {
 
     public static void init() {
-        Thread.setDefaultUncaughtExceptionHandler(new UncaughtHandler());
+        new UncaughtHandler();
     }
 
-    private static class UncaughtHandler implements Thread.UncaughtExceptionHandler {
+    static final class UncaughtHandler implements Thread.UncaughtExceptionHandler {
 
         Thread.UncaughtExceptionHandler mDefaultUncaughtHandler;
 
         public UncaughtHandler() {
-            mDefaultUncaughtHandler = Thread.getDefaultUncaughtExceptionHandler();
+            if (!(Thread.getDefaultUncaughtExceptionHandler() instanceof UncaughtHandler)) {
+                mDefaultUncaughtHandler = Thread.getDefaultUncaughtExceptionHandler();
+                Thread.setDefaultUncaughtExceptionHandler(this);
+            }
         }
 
         @Override
         public void uncaughtException(Thread t, Throwable e) {
             if ("FinalizerWatchdogDaemon".equals(t.getName()) && e instanceof TimeoutException) {
                 // ignore it. java.util.concurrent.TimeoutException - xxx.finalize()
-            } else {
-                mDefaultUncaughtHandler.uncaughtException(t, e);
+            } else if (mDefaultUncaughtHandler != Thread.getDefaultUncaughtExceptionHandler()){
+                if (mDefaultUncaughtHandler != null) {
+                    mDefaultUncaughtHandler.uncaughtException(t, e);
+                }
             }
         }
     }
-
 }
