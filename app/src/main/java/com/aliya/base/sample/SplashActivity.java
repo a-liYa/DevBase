d4f1a14af61ba47b633aeee3e63c2ffba7befaf3
a_liYa <liyafei0412@126.com>
2019-10-12 09:57:19 +0800

    优化启动页配置全屏通过主题避免闪现

--- app/src/main/java/com/aliya/base/sample/SplashActivity.java
+++ app/src/main/java/com/aliya/base/sample/SplashActivity.java
@@ -16,8 +16,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
-
 /**
  * 启动页
  *
@@ -36,7 +34,6 @@ public class SplashActivity extends BaseActivity {
             finish();
             return;
         }
-        initFullWindow();
         setSwipeBackEnable(false);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
@@ -95,33 +92,6 @@ public class SplashActivity extends BaseActivity {
             case R.id.iv_logo:
                 mIvLogo.setImageResource(R.mipmap.splash_slogan);
                 break;
-        }
-    }
-
-    /**
-     * 配置窗口属性 设置全屏
-     */
-    private void initFullWindow() {
-        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
-        if (Build.VERSION.SDK_INT >= 16) {
-            View decorView = getWindow().getDecorView();
-            // Hide both the navigation bar and the status bar.
-            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-            // a general rule, you should design your app to hide the status bar whenever you
-            // hide the navigation bar.
-//            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
-            try {
-                int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View
-                        .SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View
-                        .SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE;
-                decorView.setSystemUiVisibility(uiOptions);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 }
