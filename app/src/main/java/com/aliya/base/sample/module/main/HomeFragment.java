7db1d0938ab0a8e441057432f25c786de4cb839f
a_liYa <liyaf@8531.cn>
2020-05-27 17:53:07 +0800

    自定义 LinearDrawable 的实现

--- app/src/main/java/com/aliya/base/sample/module/main/HomeFragment.java
+++ app/src/main/java/com/aliya/base/sample/module/main/HomeFragment.java
@@ -1,25 +1,31 @@
 package com.aliya.base.sample.module.main;
 
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.aliya.base.sample.R;
 import com.aliya.base.sample.ui.activity.SecondActivity;
 import com.aliya.base.sample.ui.activity.handler.HandlerActivity;
 import com.aliya.base.sample.ui.activity.launch.SingleInstanceActivity;
 import com.aliya.base.sample.ui.activity.thread.ThreadPoolActivity;
+import com.aliya.base.sample.ui.widget.LinearDrawable;
+import com.aliya.base.sample.ui.widget.ScaleDrawable;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    private TextView mTvIcon;
 
     public HomeFragment() {
     }
@@ -37,6 +43,16 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         view.findViewById(R.id.tv_launch_mode).setOnClickListener(this);
         view.findViewById(R.id.tv_thread_pool).setOnClickListener(this);
         view.findViewById(R.id.tv_handler).setOnClickListener(this);
+        view.findViewById(R.id.tv_icon).setOnClickListener(this);
+
+        mTvIcon = view.findViewById(R.id.tv_icon);
+        LinearDrawable linearDrawable = new LinearDrawable();
+        linearDrawable.addDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.ic_icon_01));
+        linearDrawable.addDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.ic_icon_02));
+        linearDrawable.addDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.ic_icon_03));
+        linearDrawable.setBounds(0, 0, linearDrawable.getMinimumWidth(),
+                linearDrawable.getMinimumHeight());
+        mTvIcon.setCompoundDrawables(null, null, linearDrawable, null);
     }
 
     @Override
@@ -51,8 +67,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             case R.id.tv_thread_pool:
                 getActivity().startActivity(new Intent(getContext(), ThreadPoolActivity.class));
                 break;
-                case R.id.tv_handler:
+            case R.id.tv_handler:
                 getActivity().startActivity(new Intent(getContext(), HandlerActivity.class));
+                break;
+            case R.id.tv_icon:
+                Drawable[] drawables = mTvIcon.getCompoundDrawables();
+                if (drawables[2] instanceof LinearDrawable) {
+                    LinearDrawable linearDrawable = (LinearDrawable) drawables[2];
+                    ScaleDrawable scaleDrawable = new ScaleDrawable(
+                            ContextCompat.getDrawable(getContext(), R.mipmap.ic_icon_04), 1.5f);
+                    linearDrawable.addDrawable(scaleDrawable);
+                    linearDrawable.setBounds(0, 0, linearDrawable.getMinimumWidth(),
+                            linearDrawable.getMinimumHeight());
+                    mTvIcon.setCompoundDrawables(null, null, linearDrawable, null);
+                }
                 break;
         }
     }
---
+++ app/src/main/java/com/aliya/base/sample/ui/widget/LinearDrawable.java
@@ -0,0 +1,135 @@
+package com.aliya.base.sample.ui.widget;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LinearDrawable
+ *
+ * @author a_liYa
+ * @date 2020/5/27 14:26.
+ */
+public class LinearDrawable extends Drawable {
+
+    int mDrawableMargin;
+    boolean mIsVertical; // true:表示纵向
+    final List<Drawable> mDrawables = new ArrayList<>();
+
+    public LinearDrawable() {
+    }
+
+    public LinearDrawable(int drawableMargin, boolean isVertical) {
+        mDrawableMargin = drawableMargin;
+        mIsVertical = isVertical;
+    }
+
+    public void addDrawable(Drawable drawable) {
+        mDrawables.add(drawable);
+    }
+
+    public void removeDrawable(Drawable drawable) {
+        mDrawables.remove(drawable);
+    }
+
+    @Override
+    public void setBounds(int left, int top, int right, int bottom) {
+        super.setBounds(left, top, right, bottom);
+        int _left ,_top, _right, _bottom;
+         _right = _bottom = 0;
+
+        for (int i = 0; i < mDrawables.size(); i++) {
+            Drawable d = mDrawables.get(i);
+            int offset = 0;
+            if (mIsVertical) {
+                _left = left;
+                _top = _bottom;
+                if (i > 0) _top += mDrawableMargin;
+                _right = right;
+                _bottom = _top + d.getMinimumHeight();
+            } else {
+                _left = _right;
+                if (i > 0) _left += mDrawableMargin;
+                if (bottom - top > d.getMinimumHeight())
+                    offset = (int)((bottom - top - d.getMinimumHeight()) / 2f + 0.5f);
+                _top = top + offset;
+                _right = _left + d.getMinimumWidth();
+                _bottom = bottom - offset;
+            }
+
+            d.setBounds(_left, _top, _right, _bottom);
+        }
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        int intrinsicHeight = -1;
+        for (Drawable d : mDrawables) {
+            if (mIsVertical) {
+                if (d.getIntrinsicHeight() >= 0) {
+                    if (intrinsicHeight == -1)
+                        intrinsicHeight = 0;
+                    intrinsicHeight += d.getIntrinsicHeight();
+                }
+            } else {
+                intrinsicHeight = Math.max(intrinsicHeight, d.getIntrinsicHeight());
+            }
+        }
+        if (mIsVertical && mDrawables.size() > 1) {
+            intrinsicHeight += (mDrawables.size() - 1) * mDrawableMargin;
+        }
+        return intrinsicHeight;
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        int intrinsicWidth = -1;
+        for (Drawable d : mDrawables) {
+            if (mIsVertical) {
+                intrinsicWidth = Math.max(intrinsicWidth, d.getIntrinsicWidth());
+            } else {
+                if (d.getIntrinsicWidth() >= 0) {
+                    if (intrinsicWidth == -1)
+                        intrinsicWidth = 0;
+                    intrinsicWidth += d.getIntrinsicWidth();
+                }
+            }
+        }
+        if (!mIsVertical && mDrawables.size() > 1) {
+            intrinsicWidth += (mDrawables.size() - 1) * mDrawableMargin;
+        }
+        return intrinsicWidth;
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        for (Drawable d : mDrawables) {
+            d.draw(canvas);
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        for (Drawable d : mDrawables) {
+            d.setAlpha(alpha);
+        }
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        for (Drawable d : mDrawables) {
+            d.setColorFilter(colorFilter);
+        }
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
