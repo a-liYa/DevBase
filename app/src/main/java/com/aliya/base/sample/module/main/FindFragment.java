b4dc9d6702e6faf98bfca24eb9053c68379cdddb
a_liYa <liyaf@8531.cn>
2020-10-31 12:09:22 +0800

    优化 FindFragment

--- app/src/main/java/com/aliya/base/sample/module/main/FindFragment.java
+++ app/src/main/java/com/aliya/base/sample/module/main/FindFragment.java
@@ -1,14 +1,15 @@
 package com.aliya.base.sample.module.main;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.aliya.adapter.divider.GridBuilder;
 import com.aliya.base.sample.R;
 import com.aliya.base.sample.applet.MinorMainActivity;
-import com.aliya.base.sample.databinding.FragmentFindBinding;
+import com.aliya.base.sample.databinding.FragmentRecyclerLayoutBinding;
+import com.aliya.base.sample.entity.FunctionBean;
 import com.aliya.base.sample.module.image.BigImageActivity;
 import com.aliya.base.sample.module.listen.ListenNewsActivity;
 import com.aliya.base.sample.module.nested.NestedViewActivity;
@@ -17,9 +18,13 @@ import com.aliya.base.sample.ui.activity.LiveEventActivity;
 import com.aliya.base.sample.ui.activity.MineActivity;
 import com.aliya.base.sample.ui.activity.NotificationActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 /**
  * 发现页
@@ -27,56 +32,50 @@ import androidx.fragment.app.Fragment;
  * @author a_liYa
  * @date 2020-02-28 15:58.
  */
-public class FindFragment extends Fragment implements View.OnClickListener {
+public class FindFragment extends Fragment {
 
-    private FragmentFindBinding mViewBinding;
+    private FragmentRecyclerLayoutBinding mViewBinding;
+
+    private List<FunctionBean> mFunctions = new ArrayList<>();
+
+    {
+        mFunctions.add(new FunctionBean(
+                "我的", MineActivity.class, R.mipmap.icon_function_home));
+        mFunctions.add(new FunctionBean(
+                "听新闻", ListenNewsActivity.class, R.mipmap.icon_function_launch));
+        mFunctions.add(new FunctionBean(
+                "模拟通知", NotificationActivity.class, R.mipmap.icon_function_thread));
+        mFunctions.add(new FunctionBean(
+                "LiveEvent", LiveEventActivity.class, R.mipmap.icon_function_handler));
+        mFunctions.add(new FunctionBean(
+                "弱引用", WeakReferenceActivity.class, R.mipmap.icon_function_bar));
+        mFunctions.add(new FunctionBean(
+                "小程序", MinorMainActivity.class, R.mipmap.icon_function_ui));
+        mFunctions.add(new FunctionBean(
+                "大图", BigImageActivity.class, R.mipmap.icon_function_ui));
+        mFunctions.add(new FunctionBean(
+                "嵌套滑动", NestedViewActivity.class, R.mipmap.icon_function_ui));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mViewBinding = FragmentFindBinding.inflate(inflater, container, false);
-        return mViewBinding.getRoot();
+        return inflater.inflate(R.layout.fragment_recycler_layout, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        mViewBinding.tvMine.setOnClickListener(this);
-        mViewBinding.tvListen.setOnClickListener(this);
-        mViewBinding.tvNotify.setOnClickListener(this);
-        mViewBinding.tvEvent.setOnClickListener(this);
-        mViewBinding.tvWeak.setOnClickListener(this);
-        mViewBinding.tvApplet.setOnClickListener(this);
-        mViewBinding.tvBigImage.setOnClickListener(this);
-        mViewBinding.tvNested.setOnClickListener(this);
-    }
+        mViewBinding = FragmentRecyclerLayoutBinding.bind(view);
+        mViewBinding.recycler.setLayoutManager(new GridLayoutManager(getContext(), 3));
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_mine:
-                startActivity(new Intent(getContext(), MineActivity.class));
-                break;
-            case R.id.tv_listen:
-                startActivity(new Intent(getContext(), ListenNewsActivity.class));
-                break;
-            case R.id.tv_notify:
-                startActivity(new Intent(getContext(), NotificationActivity.class));
-                break;
-            case R.id.tv_event:
-                startActivity(new Intent(getContext(), LiveEventActivity.class));
-                break;
-            case R.id.tv_weak:
-                startActivity(new Intent(getContext(), WeakReferenceActivity.class));
-                break;
-            case R.id.tv_applet:
-                startActivity(new Intent(getContext(), MinorMainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                break;
-            case R.id.tv_big_image:
-                startActivity(new Intent(getContext(), BigImageActivity.class));
-                break;
-            case R.id.tv_nested:
-                startActivity(new Intent(getContext(), NestedViewActivity.class));
-                break;
-        }
+        mViewBinding.recycler.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        mViewBinding.recycler.addItemDecoration(
+                new GridBuilder(getContext())
+                        .setSpace(1)
+                        .setColorRes(R.color.color_divider)
+                        .setIncludeLineBlank(true)
+                        .setIncludeEdge(true).build()
+        );
+        mViewBinding.recycler.setAdapter(new GridFunctionAdapter(mFunctions));
     }
 }
